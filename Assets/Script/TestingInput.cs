5ab5f8e7b8a858299c59aebf11665e37ea864454
mahesh <68888458+kiddowhy@users.noreply.github.com>
2024-06-26 20:09:45 +0530

    movement added with player input c# class
    here the component is converted into c# class and all the functions are called on the main player script instead of using the player input component in the game object

--- Assets/Script/TestingInput.cs
+++ Assets/Script/TestingInput.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class TestingInput : MonoBehaviour
 {
     private Rigidbody TestRigidBody;
+    private PlayerInput playerInput;
+    private PlayerInputAction playerInputAction;
     // Start is called before the first frame update
     void Awake()
     {
         TestRigidBody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+         playerInputAction = new PlayerInputAction();
+        playerInputAction.Player.Enable();
+        playerInputAction.Player.Jump.performed += Jump;
+        playerInputAction.Player.Movement.performed += Movement_performed;
     }
 
-    // Update is called once per frame
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+        Vector3 inputVector = playerInputAction.Player.Movement.ReadValue<Vector3>();
+        float speed = 2f;
+        TestRigidBody.AddForce(new Vector3(inputVector.x, inputVector.y, inputVector.z) * speed, ForceMode.Force);
+    }
+
+
+
+    // Update is called once per frame 
     public void Jump(InputAction.CallbackContext context)
     {
+        Debug.Log(context);
         if (context.performed)
         {
             Debug.Log("jump" + context.phase);
